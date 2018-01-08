7f3b1c8b771b636da5e9dc42c46a1c3553fd8bfb
adrozdov <adrozdov2005@gmail.com>
2018-01-09 00:06:25 +0300

    Fixed express etension and hang problem

--- examples/express.js
+++ examples/express.js
@@ -4,14 +4,28 @@ var express = require('express');
 var app = express();
 
 var EctAd = require('../index');
-var ectRenderer = new EctAd({ watch: true });
+var ectRenderer = new EctAd({ext: ".html"});
 app.engine('html', ectRenderer.render);
 app.set('view engine', 'html');
 app.set('views', __dirname + '/view');
 
 app.get('/', function (req, res){
-    res.render('page.html');
+    res.render('page');
 });
+
+app.get('/simple', function (req, res) {
+    res.send("Done!");
+});
+
+app.get('/other', function (req, res) {
+    res.render("other");
+});
+
+
+app.use(function(err, req, res, next) {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+  });
 
 app.listen(3000);
 console.log('Listening on port 3000');
